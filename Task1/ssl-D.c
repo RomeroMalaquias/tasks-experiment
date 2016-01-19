a93a357a8b96c24e145aca744023c310c32b9717
Romero Malaquias <romero.malaquias@gmail.com>
2016-01-19 03:12:06 -0300

    1a. task

--- Task1/ssl-D.c
+++ Task1/ssl-D.c
@@ -1,12 +1,81 @@
-  if ((session->opt->ssl_flags & SSLF_USERNAME_AS_COMMON_NAME))
-  	    set_common_name (session, up->username);
+#include <stdbool.h>
+#include <stdio.h>
+#define D_HANDSHAKE 1
+#define SSLF_USERNAME_AS_COMMON_NAME 1
+#define ENABLE_DEF_AUTH
+
+
+typedef struct opts
+{
+	int ssl_flags;
+} opts;
+#ifdef ENABLE_DEF_AUTH
+typedef struct auth_string
+{
+	char auth_deferred[20];
+} auth_string;
+#endif
+
+typedef struct tls_session
+{
+	struct opts opt;
+	char username[20];
+} tls_session;
+
+typedef struct user
+{
+	char username[20];
+} user; 
+
+void msg (int handshake, char msg[], char return_msg[], char username[], char type_set[])
+{
+	bool is_authenticated = handshake;	
+#  ifdef ENABLE_DEF_AUTH
+        is_authenticated = is_authenticated && return_msg != "deferred";
+#	endif
+#  ifdef PLUGIN_DEF_AUTH
+        is_authenticated = is_authenticated && type_set != "[CN SET]";
+#  endif
+#  ifdef ENABLE_OCC
+    is_authenticated = is_authenticated || (return_msg == "succeeded" && username.length > 0);
+#  endif
+    if (is_authenticated) 
+	{
+		printf("authentication succeeded");
+	} else {
+		printf("authentication failed");
+	}
+	
+}
+void set_common_name (struct tls_session *session, char username[]) 
+{	
+}
+
+static bool key_method_2_read (struct tls_session *session, struct user up, struct auth_string ks)
+{  	  
+  if ((session->opt.ssl_flags & SSLF_USERNAME_AS_COMMON_NAME))
+  	    set_common_name (session, up.username);
 #ifdef ENABLE_DEF_AUTH
 	  msg (D_HANDSHAKE, "TLS: Username/Password authentication %s for username '%s' %s",
-  	       ks->auth_deferred ? "deferred" : "succeeded",
-	       up->username,
-	       (session->opt->ssl_flags & SSLF_USERNAME_AS_COMMON_NAME) ? "[CN SET]" : "");
+  	       ks.auth_deferred ? "deferred" : "succeeded",
+	       up.username,
+	       (session->opt.ssl_flags & SSLF_USERNAME_AS_COMMON_NAME) ? "[CN SET]" : "");
 #else
 	  msg (D_HANDSHAKE, "TLS: Username/Password authentication %s for username '%s' %s","succeeded",
-  	       up->username,
-  	       (session->opt->ssl_flags & SSLF_USERNAME_AS_COMMON_NAME) ? "[CN SET]" : "");
+  	       up.username,
+  	       (session->opt.ssl_flags & SSLF_USERNAME_AS_COMMON_NAME) ? "[CN SET]" : "");
 #endif
+return true;
+}
+
+int main() {
+opts session_opts = {0};
+tls_session session = {session_opts, ""};
+user up = {"joao"};
+#ifdef ENABLE_DEF_AUTH
+auth_string ks = {"succeeded"};
+#endif
+key_method_2_read(&session, up, ks);
+
+return 0;
+}
--- Task1/ssl-UD.c
+++ Task1/ssl-UD.c
@@ -2,42 +2,71 @@
 #include <stdio.h>
 #define D_HANDSHAKE 1
 #define SSLF_USERNAME_AS_COMMON_NAME 1
+#define ENABLE_DEF_AUTH
 
 
-struct opts
+typedef struct opts
 {
 	int ssl_flags;
-};
+} opts;
+#ifdef ENABLE_DEF_AUTH
+typedef struct auth_string
+{
+	char auth_deferred[20];
+} auth_string;
+#endif
 
-struct tls_session
+typedef struct tls_session
 {
 	struct opts opt;
-};
+	char username[20];
+} tls_session;
 
-struct user
+typedef struct user
 {
 	char username[20];
-};
+} user;
 
 
 
 void msg (int handshake, char msg[], char return_msg[], char username[], char type_set[])
 {
-	printf("olá");
+	if (handshake && 
+#  ifdef ENABLE_DEF_AUTH
+        return_msg != "deferred"
+#	endif
+#   if defined(PLUGIN_DEF_AUTH) || defined(ENABLE_OCC)
+        &&
+#   endif
+#  ifdef PLUGIN_DEF_AUTH
+        type_set != "[CN SET]"
+#   ifdef ENABLE_OCC
+        ||
+#   endif
+#  endif
+#  ifdef ENABLE_OCC
+    (return_msg == "succeeded" && username.length > 0)
+#  endif
+    ) 
+	{
+		printf("authentication succeeded");
+	} else {
+		printf("authentication failed");
+	}
+	
 }
 
 void set_common_name (struct tls_session *session, char username[]) 
-{
+{	
 }
 
-static bool key_method_2_read (struct tls_session *session)
-{  
-	struct user up = {"joao"};
+static bool key_method_2_read (struct tls_session *session, struct user up, struct auth_string ks)
+{  	
 	if ((session->opt.ssl_flags & SSLF_USERNAME_AS_COMMON_NAME))
   	    set_common_name (session, up.username);
             msg (D_HANDSHAKE, "TLS: Username/Password authentication %s for username '%s' %s",
 #ifdef ENABLE_DEF_AUTH
-  	       ks->auth_deferred ? "deferred" : "succeeded",
+  	       ks.auth_deferred ? "deferred" : "succeeded",
 #else
   	       "succeeded",
 #endif
@@ -47,8 +76,13 @@ return true;
 }
 
 int main() {
-struct opts session_opts = {0};
-struct tls_session session = {session_opts};
-key_method_2_read(&session);
-return 1;
+opts session_opts = {0};
+tls_session session = {session_opts, ""};
+user up = {"joao"};
+#ifdef ENABLE_DEF_AUTH
+auth_string ks = {"succeeded"};
+#endif
+key_method_2_read(&session, up, ks);
+
+return 0;
 }
