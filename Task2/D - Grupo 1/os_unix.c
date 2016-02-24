66982c5aa65bf9c5c13a26e287eb739bdda3308a
Romero B. de S. Malaquias <romero.malaquias@gmail.com>
2016-02-24 13:30:35 -0300

    Update os_unix.c

--- Task2/D - Grupo 1/os_unix.c
+++ Task2/D - Grupo 1/os_unix.c
@@ -6,14 +6,15 @@
 #define FEAT_MZSCHEME
 #define HAVE_SYS_TIME_H
 #define HAVE_GETTIMEOFDAY
+#define USE_XSMP
 
 int 
 mzthreads_allowed() 
 {
 	return 1;
 };
-int
 
+int
 RealWaitForChar(fd, msec, check_for_gpm)
     int     fd;
     long    msec;
@@ -21,11 +22,12 @@ RealWaitForChar(fd, msec, check_for_gpm)
 {
     int     ret;
 #ifdef FEAT_NETBEANS_INTG
-    int     nb_fd = netbeans_filedesc();
+    int     nb_fd = TRUE;
 #endif
 #if defined(FEAT_XCLIPBOARD) || defined(USE_XSMP) || defined(FEAT_MZSCHEME)
     static int  busy = FALSE;
 	int p_mzq = TRUE;
+	int xsmp_icefd = 10;
 
     /* May retry getting characters after an event was handled. */
 #define MAY_LOOP
@@ -37,29 +39,29 @@ RealWaitForChar(fd, msec, check_for_gpm)
  	int update_time, start_tv = 0;
 	
 #ifdef FEAT_XCLIPBOARD
-        update_time = xterm_Shell != (Widget)0;
+	update_time = xterm_Shell != (Widget)0;
 #ifdef USE_XSMP
-        update_time = update_time || xsmp_icefd != -1;
+	update_time = update_time || xsmp_icefd != -1;
 #endif
 #ifdef FEAT_MZSCHEME
-		update_time = update_time || (mzthreads_allowed() && p_mzq > 0);
+	update_time = update_time || (mzthreads_allowed() && p_mzq > 0);
 #endif
 #else
 #ifdef USE_XSMP
-        update_time = xsmp_icefd != -1;
+	update_time = xsmp_icefd != -1;
 #ifdef FEAT_MZSCHEME
-			update_time = update_time || (mzthreads_allowed() && p_mzq > 0);
+	update_time = update_time || (mzthreads_allowed() && p_mzq > 0);
 #endif
 #else
 #ifdef FEAT_MZSCHEME
-			update_time = ((mzthreads_allowed() && p_mzq > 0);
+	update_time = ((mzthreads_allowed() && p_mzq > 0);
 #endif
 #endif		
 	
 #endif
 
-        if(msec > 0 &&update_time)
-    gettimeofday(&start_tv, NULL);
+	if(msec > 0 && update_time)
+		msec = 0;
 #endif
 
     /* Handle being called recursively.  This may happen for the session
@@ -68,10 +70,4 @@ RealWaitForChar(fd, msec, check_for_gpm)
 		return 0;
 # endif
 	return 1;
-}
-
-int main() {
-
-
-return 0;
 }
