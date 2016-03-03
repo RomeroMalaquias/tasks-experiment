a10eb17666f41f8475aa080a17aa6c19cd8600f4
lzmths <luizmatheus.ac@gmail.com>
2016-03-03 12:16:08 -0300

    Update evutil.c

--- Task3/UD - Grupo 2/evutil.c
+++ Task3/UD - Grupo 2/evutil.c
@@ -5,12 +5,15 @@ int
 evutil_ersatz_socketpair_(int family, int type, int protocol)
 {
 	if (protocol
+#ifdef USE_AF_INET
 		|| (
 			family != AF_INET
-	#ifdef USE_AF_UNIX
+#ifdef USE_AF_UNIX
 			&& family != AF_UNIX
-	#endif
-		)) {
+#endif
+		)
+#endif
+		) {
 			return -1;
 	}
 
